e4e31c260a0e69a72de829a228dbff68bb92ffa3
Bombo <patryk080998@gmail.com>
2025-10-18 22:47:00 +0900

    Added types for mapping api responses

---
+++ types/weather.ts
@@ -0,0 +1,189 @@
+export type WeatherLocation = {
+    name: string
+    region: string
+    country: string
+    lat: number
+    lon: number
+    tz_id: string
+    localtime_epoch: number
+    localtime: string
+};
+
+export type CurrentWeather = {
+    last_updated_epoch: number
+    last_updated: string
+    temp_c: number
+    temp_f: number
+    is_day: number
+    condition: Condition
+    wind_mph: number
+    wind_kph: number
+    wind_degree: number
+    wind_dir: string
+    pressure_mb: number
+    pressure_in: number
+    precip_mm: number
+    precip_in: number
+    humidity: number
+    cloud: number
+    feelslike_c: number
+    feelslike_f: number
+    vis_km: number
+    vis_miles: number
+    uv: number
+    gust_mph: number
+    gust_kph: number
+    air_quality: number
+    co: number
+    no2: number
+    o3: number
+    so2: number
+    pm2_5: number
+    pm10: number
+    "us-epa-index": number
+    "gb-defra-index": number
+};
+
+export type Forecast = {
+    date: string
+    date_epoch: number
+    day: {
+        maxtemp_c: number
+        maxtemp_f: number
+        mintemp_c: number
+        mintemp_f: number
+        avgtemp_c: number
+        avgtemp_f: number
+        maxwind_mph: number
+        maxwind_kph: number
+        totalprecip_mm: number
+        totalprecip_in: number
+        avgvis_km: number
+        avgvis_miles: number
+        avghumidity: number
+        daily_will_it_rain: number
+        daily_chance_of_rain: number
+        daily_will_it_snow: number
+        daily_chance_of_snow: number
+        condition: Condition
+        uv: number
+    },
+    astro: Astronomy
+    hour: [{
+        time_epoch: number
+        time: string
+        temp_c: number
+        temp_f: number
+        is_day: number
+        condition: Condition
+        wind_mph: number
+        wind_kph: number
+        wind_degree: number
+        wind_dir: string
+        pressure_mb: number
+        pressure_in: number
+        precip_mm: number
+        precip_in: number
+        humidity: number
+        cloud: number
+        feelslike_c: number
+        feelslike_f: number
+        windchill_c: number
+        windchill_f: number
+        heatindex_c: number
+        heatindex_f: number
+        dewpoint_c: number
+        dewpoint_f: number
+        will_it_rain: number
+        chance_of_rain: number
+        will_it_snow: number
+        chance_of_snow: number
+        vis_km: number
+        vis_miles: number
+        gust_mph: number
+        gust_kph: number
+        uv: number
+    }]
+};
+
+export type Condition = {
+    text: string
+    icon: string
+    code: number
+};
+ 
+export type Alert = {
+    headline: string
+    msgtype: string | null
+    severity: string | null
+    urgency: string | null
+    areas: string | null
+    category: string
+    certainty: string | null
+    event: string
+    note: string | null
+    effective: string
+    expires: string
+    desc: string
+    instruction: string
+};
+
+export type Astronomy = {
+    sunrise: string
+    sunset: string
+    moonrise: string
+    moonset: string
+    moon_phase: string
+    moon_illumination: string
+};
+
+export type Search = {
+    id: number
+    name: string
+    region: string
+    country: string
+    lat: number
+    lon: number
+    url: string
+};
+
+export type TimeZone = {
+    name: string
+    region: string
+    country: string
+    lat: number
+    lon: number
+    tz_id: string
+    localtime_epoch: number
+    localtime: string
+};
+
+export type CurrentResponse = {
+    location: WeatherLocation
+    current: CurrentWeather
+};
+
+export type ForecastResponse = {
+    location: WeatherLocation
+    current: CurrentWeather
+    forecast: Forecast[]
+    alerts: Alert[]
+};
+
+export type FutureHistoryResponse = {
+    location: WeatherLocation
+    forecast: Forecast[]
+};
+
+export type SearchResponse = {
+    search: Search[]
+};
+
+export type TimezoneResponse = {
+    timezone: TimeZone
+};
+
+export type AstronomyResponse = {
+    location: WeatherLocation
+    astronomy: Astronomy
+};
